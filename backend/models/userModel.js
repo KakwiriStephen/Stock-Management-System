640805eba9916ac7324eaa12f4f82ca47636cb17
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-12 19:45:17 +0300

    Salting

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -50,7 +50,7 @@ userSchema.pre("save", async function (next) {
   //Hash password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(this.password, salt);
-  this.passowrd = hashedPassword;
+  this.password = hashedPassword;
 });
 
 const User = mongoose.model("User", userSchema);
