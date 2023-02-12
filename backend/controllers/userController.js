c40effed81e60b42e6788e6c04d712c938319244
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-12 16:39:06 +0300

    Creating user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,6 +22,29 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Email has already been registered");
   }
+
+  //create User
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(201).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 module.exports = {
