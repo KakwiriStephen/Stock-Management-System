0007712ac53697050d7245c08bb16df2009b7b14
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-12 16:23:47 +0300

    Validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,28 @@
-const registerUser = async (req, res) => {
-  res.send("Register User");
-};
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
+  //destructuring
+  const { name, email, password } = req.body;
+
+  //validation
+  if (!name || email || password) {
+    res.status(400);
+    throw new Error("please fill in all required fields");
+  }
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error("Password must be upto 6 characters");
+  }
+
+  //check if user email alredy exist
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("Email has already been registered");
+  }
+});
 
 module.exports = {
   registerUser,
