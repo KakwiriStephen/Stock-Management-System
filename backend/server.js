192e20cb68cad978e8bc1291fedcc1e9007da847
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-12 13:21:54 +0300

    MONGODB

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,19 @@ const dotenv = require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const app = express();
 
-const app=express()
+const PORT = process.env.PORT || 5000;
 
+//connecting to database
+mongoose.set("strictQuery", false);
 
-const PORT=process.env.PORT || 5000
+mongoose
+  .connect(process.env.MONGO_URI)
+
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server runnig on port ${PORT}`);
+    });
+  })
+  .catch((err) => comsole.log(err));
