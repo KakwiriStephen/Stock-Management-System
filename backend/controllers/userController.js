c7e5f499941673040c1cb5f99894b19acd00cf7c
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-13 11:30:26 +0300

    Login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -86,6 +86,18 @@ const loginUser = asyncHandler(async (req, res) => {
   //check if password is correct
   const passwordIsCorrect = await bcrypt.compare(password, user.password);
 
+  //Generate Token
+  const token = generateToken(user._id);
+
+  //Send HTTP-only cookie
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400), //1 day
+    sameSite: "none",
+    secure: true,
+  });
+
   if (user && passwordIsCorrect) {
     const { _id, name, email, photo, phone, bio } = user;
     res.status(200).json({
