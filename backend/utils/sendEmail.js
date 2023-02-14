a5afb2acaa16574d0081ada0b75ea9352bc467be
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-14 16:00:32 +0300

    Nodemailer transporter

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,17 @@
+const nodemailer = require("nodemailer");
+
+const sendMail = async (req, res) => {
+  //Create Transporter
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+};
