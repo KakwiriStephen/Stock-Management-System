fea39aa5441db2cc4f185a0449e573f28b283c3e
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-15 22:16:08 +0300

    Product Model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,17 +1,54 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+      trim: true,
+    },
+    sku: {
+      type: String,
+      required: true,
+      default: "SKU",
+      trim: true,
+    },
+
+    category: {
+      type: String,
+      required: [true, "Please add a category"],
+      trim: true,
+    },
+
+    quantity: {
+      type: string,
+      required: [true, "Please add a quantity"],
+      trim: true,
+    },
+    price: {
+      type: String,
+      required: [true, "A please add a price"],
+      true: trim,
+    },
+    description: {
+      type: String,
+      required: [true, "Please add a description"],
+      trim: true,
+    },
+    image: {
+      type: Object,
+      default: {},
+    },
   },
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
-    trim: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Product = mongoose.model("Product", productSchema);
 
