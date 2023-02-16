6bb57487e0ec1d18c9e5dbab8602bc7f3408be4d
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-16 12:25:26 +0300

    File Upload

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const Product = require("../models/productModel");
+const cloudinary = require("cloudinary").v2;
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
 const createProduct = asyncHandler(async (req, res) => {
   const { name, sku, category, quantity, price, description } = req.body;
--- backend/utils/fileUpload.js
+++ backend/utils/fileUpload.js
@@ -1,1 +1,14 @@
 const multer = require("multer");
+
+//Define file Storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      new Date().toISOString.replace(/:/g, +"-") + "-" + file.originalname
+    ); //Add date to file
+  },
+});
