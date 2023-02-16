c750d2beded815fdc3a7eae763d56ca4c5ed62b3
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-16 12:02:23 +0300

    Creating Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,14 @@ const asyncHandler = require("express-async-handler");
 const Product = require("../models/productModel");
 
 const createProduct = asyncHandler(async (req, res) => {
-  res.send("On to the products!");
+  const { name, sku, category, quantity, price, description } = req.body;
+
+  //Validation
+
+  if (!name || !category || !sku || !quantity || !price || !description) {
+    res.status(400);
+    throw new Error("Please fill in all the required fields");
+  }
 });
 
 module.exports = { createProduct };
