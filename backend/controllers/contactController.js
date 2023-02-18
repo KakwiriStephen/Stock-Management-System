66367735bc4e50d43c1698722ff30936bce36069
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-18 13:22:33 +0300

    Contact

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -17,6 +17,17 @@ const contactUs = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add subject and message");
   }
+
+  const send_to = process.env.EMAIL_USER;
+  const sent_from = process.env.EMAIL_USER;
+  const reply_to = user.email;
+  try {
+    await sendEmail(subject, message, send_to, sent_from, reply_to);
+    res.status(200).json({ success: true, message: "Email Sent" });
+  } catch (error) {
+    res.status(500);
+    throw new Error("Email not sent, please try again");
+  }
 });
 
 module.exports = { contactUs };
