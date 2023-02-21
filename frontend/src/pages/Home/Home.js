96fc1c20481de08f5ea4601f650e188166bf8672
KakwiriStephen <ngottakakwiri@gmail.com>
2023-02-21 09:03:51 +0300

    Frontend

---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { IoLogoIonic } from "react-icons/io";
+import { Link } from "react-router-dom";
+import "./Home.scss";
+import heroImg from "../../assets/inv-img1.png";
+import { ShowOnLogin, ShowOnLogout } from "../../components/protect/HiddenLink";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <nav className="container --flex-between ">
+        <div className="logo">
+          <IoLogoIonic size={35} />
+        </div>
+
+        <ul className="home-links">
+          <ShowOnLogout>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogout>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/login">Login</Link>
+              </button>
+            </li>
+          </ShowOnLogout>
+          <ShowOnLogin>
+            <li>
+              <button className="--btn --btn-primary">
+                <Link to="/dashboard">Dashboard</Link>
+              </button>
+            </li>
+          </ShowOnLogin>
+        </ul>
+      </nav>
+      {/* HERO SECTION */}
+      <section className="container hero">
+        <div className="hero-text">
+          <h2>Inventory {"&"} Stock Management Solution</h2>
+          <p>
+            We offer an advanced inventory system that enables real-time control
+            and management of products in your warehouse. Our system is fully
+            integrated, making it easier to develop and grow your business.
+          </p>
+          <div className="hero-buttons">
+            <button className="--btn --btn-secondary">
+              <Link to="/dashboard">Free Trial 1 Month</Link>
+            </button>
+          </div>
+          <div className="--flex-start">
+            <NumberText num="14K" text="Brand Owners" />
+            <NumberText num="23K" text="Active Users" />
+            <NumberText num="500+" text="Partners" />
+          </div>
+        </div>
+
+        <div className="hero-image">
+          <img src={heroImg} alt="Inventory" />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+const NumberText = ({ num, text }) => {
+  return (
+    <div className="--mr">
+      <h3 className="--color-white">{num}</h3>
+      <p className="--color-white">{text}</p>
+    </div>
+  );
+};
+
+export default Home;
